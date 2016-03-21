2818aa0bd0abb4645acf9bffd76426095f257cb5
falood <falood@gmail.com>
2016-03-21 19:26:59 +0800

    change options of ets tables

--- lib/mem/supervisor.ex
+++ lib/mem/supervisor.ex
@@ -2,15 +2,15 @@ defmodule Mem.Supervisor do
   use Supervisor
 
   def start_link({names, mem_size, mem_strategy, module}) do
-    :ets.new(names.proxy_ets, [:set, :public, :named_table, read_concurrency: true])
-    :ets.new(names.data_ets,  [:set, :public, :named_table, write_concurrency: true])
-    :ets.new(names.ttl_ets,   [:set, :public, :named_table, write_concurrency: true])
+    :ets.new(names.proxy_ets, [:set, :public, :named_table, :compressed, read_concurrency: true])
+    :ets.new(names.data_ets,  [:set, :public, :named_table, :compressed, write_concurrency: true])
+    :ets.new(names.ttl_ets,   [:set, :public, :named_table, :compressed, write_concurrency: true])
     try do
       Supervisor.start_link(__MODULE__, [names, module], name: names.sup_name)
     after
       unless is_nil(mem_size) do
-        :ets.new(names.lru_ets, [:set, :public, :named_table, write_concurrency: true])
-        :ets.new(names.lru_inverted_ets, [:ordered_set, :public, :named_table, write_concurrency: true])
+        :ets.new(names.lru_ets, [:set, :public, :named_table, :compressed])
+        :ets.new(names.lru_inverted_ets, [:ordered_set, :public, :named_table, :compressed])
         cleaner =
           case mem_strategy do
             :lru  -> Mem.Cleaners.LRU
