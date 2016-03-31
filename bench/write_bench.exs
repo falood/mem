2c1dc628ba0cefe943add0b850305a2d5533eda3
falood <falood@gmail.com>
2016-03-31 18:53:35 +0800

    more benches

--- bench/write_bench.exs
+++ bench/write_bench.exs
@@ -2,6 +2,18 @@ defmodule BenchWrite do
   use Mem, worker_number: 2
 end
 
+defmodule BenchWrite.Persistence do
+  use Mem, worker_number: 2, persistence: true
+end
+
+defmodule BenchWrite.LRU do
+  use Mem, worker_number: 2, maxmemory_size: "100M"
+end
+
+defmodule BenchWrite.Persistence.LRU do
+  use Mem, worker_number: 2, maxmemory_size: "100M", persistence: true
+end
+
 defmodule BenchWrite.Supervisor do
   use Supervisor
 
@@ -15,6 +27,9 @@ defmodule BenchWrite.Supervisor do
 
   def init([]) do
     [ BenchWrite.child_spec,
+      BenchWrite.Persistence.child_spec,
+      BenchWrite.LRU.child_spec,
+      BenchWrite.Persistence.LRU.child_spec,
     ] |> supervise(strategy: :one_for_one)
   end
 
@@ -24,6 +39,15 @@ defmodule WriteBench do
   use Benchfella
 
   setup_all do
+  #   bench_dir = "/tmp/mem/write_bench"
+  #   File.rm_rf!(bench_dir)
+  #   File.mkdir_p!(bench_dir)
+  #   Application.unload(:mnesia)
+  #   Application.load(:mnesia)
+  #   Application.put_env(:mnesia, :dir, to_char_list(bench_dir))
+  #   Application.put_env(:mnesia, :dc_dump_limit, 40)
+  #   Application.put_env(:mnesia, :dump_log_write_threshold, 50000)
+
     :ets.new(:bench_write, [:set, :public, :named_table, write_concurrency: true])
     BenchWrite.Supervisor.start_link
   end
@@ -32,15 +56,33 @@ defmodule WriteBench do
     BenchWrite.Supervisor.stop
   end
 
-  bench "bench Mem write" do
-    Enum.each(1..1_000_000, fn x ->
-      BenchWrite.set(x, x)
+  bench "bench ETS write" do
+    Enum.each(1..100_000, fn x ->
+      :ets.insert(:bench_write, {x, x})
     end)
   end
 
-  bench "bench ETS write" do
-    Enum.each(1..1_000_000, fn x ->
-      :ets.insert(:bench_write, {x, x})
+  bench "bench Mem write" do
+    Enum.each(1..100_000, fn x ->
+      BenchWrite.set(x, x, 200)
+    end)
+  end
+
+  bench "bench Mem write with Persistence" do
+    Enum.each(1..100_000, fn x ->
+      BenchWrite.Persistence.set(x, x)
+    end)
+  end
+
+  bench "bench Mem write with LRU" do
+    Enum.each(1..100_000, fn x ->
+      BenchWrite.LRU.set(x, x)
+    end)
+  end
+
+  bench "bench Mem write with Persistence and LRU" do
+    Enum.each(1..100_000, fn x ->
+      BenchWrite.Persistence.LRU.set(x, x)
     end)
   end
 
